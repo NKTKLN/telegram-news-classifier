f6755351f0c17832b9c53b25324ca471311beb16
NKTKLN <nktkln@nktkln.com>
2024-12-24 18:44:20 +0300

    Added function to update some config fields

--- bot/config.py
+++ bot/config.py
@@ -2,9 +2,13 @@ import yaml
 
 class Config:
     def __init__(self, config_path: str):
-        with open(config_path, 'r', encoding='utf-8') as file:
-            self._config = yaml.safe_load(file)
+        self.config_path = config_path
+        self._load_config()
     
+    def _load_config(self):
+        with open(self.config_path, 'r', encoding='utf-8') as file:
+            self._config = yaml.safe_load(file)
+
     @property
     def telegram(self):
         return self._config.get('telegram', {})
@@ -30,3 +34,32 @@ class Config:
     
     def get_telegram_channels(self):
         return self.telegram.get('channels', [])
+
+    def add_telegram_channel(self, channel_id: int, name: str = ""):
+        if 'telegram' not in self._config:
+            self._config['telegram'] = {}
+        if 'channels' not in self._config['telegram']:
+            self._config['telegram']['channels'] = []
+        
+        existing_channels = self.get_telegram_channels()
+        if not any(channel['id'] == channel_id for channel in existing_channels):
+            self._config['telegram']['channels'].append({'id': channel_id, 'name': name})
+            self._save_config()
+            self._load_config()
+
+    def add_telegram_topic(self,  topic_id: int, topic_topic_id: int):
+        if 'telegram' not in self._config:
+            self._config['telegram'] = {}
+        if 'topics' not in self._config['telegram']:
+            self._config['telegram']['topics'] = []
+        
+        existing_topics = self.get_telegram_topics()
+        if not any(topic['id'] == topic_id for topic in existing_topics):
+            self._config['telegram']['topics'].append({'id': topic_id, 'topic_id': topic_topic_id})
+            self._save_config()
+            self._load_config()
+
+    def _save_config(self):
+        """Сохраняет текущую конфигурацию в файл."""
+        with open(self.config_path, 'w', encoding='utf-8') as file:
+            yaml.safe_dump(self._config, file, default_flow_style=False, allow_unicode=True)
